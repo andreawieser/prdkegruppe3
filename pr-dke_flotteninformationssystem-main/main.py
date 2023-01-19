b41dfe94ec0ff74ba4513aa3e10f5d5b2123c28b
Markus <66691927+4Sikex@users.noreply.github.com>
2023-01-19 14:28:43 +0100

    Update port in main.py

--- pr-dke_flotteninformationssystem-main/main.py
+++ pr-dke_flotteninformationssystem-main/main.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == '__main__':
     # run the app in debug mode to auto-reload
     # app.run(debug=True)
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
