1872f3db0b3d5c2e60b5280ed3540f491c3a3122
andreawieser <79004494+andreawieser@users.noreply.github.com>
2023-01-24 14:48:52 +0100

    Delete ticket directory

--- ticket/ticketsMain.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-from app.models import User
-
-# Gibt den Shell-Kontext für eine interaktive Shell zurück
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User}
